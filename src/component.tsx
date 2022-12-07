c9df6d107f2a66710b9be95dcfaa8c1f5c5e89db
stagas <gstagas@gmail.com>
2022-12-07 19:12:20 +0200

    version 3.0.6

--- src/component.tsx
+++ src/component.tsx
@@ -9,20 +9,21 @@ const IconSvgTag = $.element(IconSvgElement)
 export const IconSvg = <P extends keyof IconSets<IconSetsNames>, T extends IconTypes[P & keyof IconTypes]>(
   props:
     & (P extends keyof IconKinds ? {
-        set: P
-        icon: Icons[P]
-        kind: IconKinds[P & keyof IconKinds]
-      }
+      set: P
+      icon: Icons[P]
+      kind: IconKinds[P & keyof IconKinds]
+    }
       : P extends keyof IconTypes ? {
-          set: P
-          icon: IconsWithTypes[P & keyof IconsWithTypes][T & keyof IconsWithTypes[P & keyof IconsWithTypes]]
-          type: T
-        }
+        set: P
+        icon: IconsWithTypes[P & keyof IconsWithTypes][T & keyof IconsWithTypes[P & keyof IconsWithTypes]]
+        type: T
+      }
       : {
         set: P
         icon: Icons[P & keyof Icons]
       })
     & $.HTMLAttributes<IconSvgElement>,
+  // @ts-ignore
 ): JSX.Element => <IconSvgTag {...props}></IconSvgTag>
 
 IconSvg.toString = IconSvgTag.toString
